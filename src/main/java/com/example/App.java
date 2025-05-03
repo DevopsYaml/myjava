defe7eabd80e7fe973e757cd037fcec474c5b65e
champ <raj.durgesh@outlook.com>
2025-05-03 21:08:18 +0530

    New text added for webhook

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Jenkins! How are you");
+        System.out.println("Hello All, i have triggered using jenkins and github webhook");
     }
 }
